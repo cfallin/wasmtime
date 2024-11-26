b29ca1d5d8ea2d75f669234f20d969df8e0f7e2d
Chris Fallin <chris@cfallin.org>
2024-11-25 22:13:08 -0800

    WIP: order graph.

--- cranelift/codegen/src/ir/ordergraph.rs
+++ cranelift/codegen/src/ir/ordergraph.rs
@@ -46,9 +46,13 @@
 //! least some offset? We compute the result of this query by
 //! exploring outward.
 
+use std::collections::hash_map::Entry;
+
 use crate::entity::{entity_impl, PrimaryMap};
+use crate::unionfind::UnionFind;
 use alloc::collections::BTreeSet;
 use alloc::vec::Vec;
+use rustc_hash::FxHashMap;
 
 /// A node in the order graph.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -74,12 +78,46 @@ pub struct OrderGraph {
 /// Data at one node in the order graph.
 #[derive(Clone, Debug)]
 pub struct OrderNodeData {
-    /// Known lower-bound constant value, if any.
-    pub lo: Option<u64>,
-    /// Known upper-bound constant value, if any.
-    pub hi: Option<u64>,
+    /// Constant bound (equal or upper bound), if any.
+    pub bound: ConstantBound,
     /// Variables in the equivalence class represented by this node.
     pub exprs: Vec<OrderVar>,
+}
+
+/// A constant-valued bound.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ConstantBound {
+    /// No constant bound known.
+    None,
+    /// Equal to the given value.
+    Eq(u64),
+    /// Less than or equal to the given value.
+    Le(u64),
+}
+
+impl ConstantBound {
+    fn merge(a: ConstantBound, b: ConstantBound) -> Option<ConstantBound> {
+        match (a, b) {
+            (ConstantBound::None, b) => Some(b),
+            (a, ConstantBound::None) => Some(a),
+            (ConstantBound::Eq(a), ConstantBound::Eq(b)) if a == b => Some(ConstantBound::Eq(a)),
+            (ConstantBound::Le(a), ConstantBound::Eq(b))
+            | (ConstantBound::Eq(b), ConstantBound::Le(a))
+                if b <= a =>
+            {
+                Some(ConstantBound::Eq(b))
+            }
+            (ConstantBound::Le(a), ConstantBound::Le(b)) => {
+                let x = std::cmp::min(a, b);
+                if x == 0 {
+                    Some(ConstantBound::Eq(0))
+                } else {
+                    Some(ConstantBound::Le(x))
+                }
+            }
+            _ => None,
+        }
+    }
 }
 
 /// An ordering edge from one node to another.
@@ -93,6 +131,50 @@ pub struct OrderEdge {
     pub kind: OrderEdgeKind,
     /// The offset (distance) denoted by this edge.
     pub offset: u64,
+}
+
+impl OrderEdge {
+    fn compose_left(lhs_bound: &ConstantBound, edge: &OrderEdge) -> ConstantBound {
+        match (lhs_bound, edge.kind) {
+            (ConstantBound::None, _) => ConstantBound::None,
+            (_, OrderEdgeKind::Le) => ConstantBound::None,
+            (ConstantBound::Eq(x), OrderEdgeKind::Eq) => {
+                ConstantBound::Eq(x.saturating_add(edge.offset))
+            }
+            (ConstantBound::Le(x), OrderEdgeKind::Eq) => {
+                ConstantBound::Le(x.saturating_add(edge.offset))
+            }
+        }
+    }
+    fn compose_right(edge: &OrderEdge, rhs_bound: &ConstantBound) -> ConstantBound {
+        match (edge.kind, rhs_bound) {
+            (_, ConstantBound::None) => ConstantBound::None,
+            (OrderEdgeKind::Eq, ConstantBound::Eq(x)) => {
+                ConstantBound::Eq(x.saturating_sub(edge.offset))
+            }
+            (OrderEdgeKind::Eq, ConstantBound::Le(x))
+            | (OrderEdgeKind::Le, ConstantBound::Eq(x))
+            | (OrderEdgeKind::Le, ConstantBound::Le(x)) => {
+                ConstantBound::Le(x.saturating_sub(edge.offset))
+            }
+        }
+    }
+    fn merge(a: &OrderEdge, b: &OrderEdge) -> Option<OrderEdge> {
+        assert_eq!(a.lhs, b.lhs);
+        assert_eq!(a.rhs, b.rhs);
+        match (a.kind, b.kind) {
+            (OrderEdgeKind::Eq, OrderEdgeKind::Eq) if a.offset == b.offset => Some(a.clone()),
+            (OrderEdgeKind::Le, OrderEdgeKind::Le) => Some(OrderEdge {
+                lhs: a.lhs,
+                rhs: a.rhs,
+                kind: OrderEdgeKind::Le,
+                offset: std::cmp::max(a.offset, b.offset),
+            }),
+            (OrderEdgeKind::Le, OrderEdgeKind::Eq) if b.offset >= a.offset => Some(b.clone()),
+            (OrderEdgeKind::Eq, OrderEdgeKind::Le) if a.offset >= b.offset => Some(a.clone()),
+            _ => None,
+        }
+    }
 }
 
 /// An edge kind: equal or less-than-or-equal.
@@ -132,11 +214,15 @@ pub enum OrderNodeOrConst {
 }
 
 impl OrderGraph {
-    /// Allocate a fresh variable.
-    pub fn fresh_var(&mut self) -> OrderVar {
+    /// Allocate a fresh variable and create a new order node for it.
+    pub fn fresh_var(&mut self) -> OrderNode {
         let index = self.next_var;
         self.next_var += 1;
-        OrderVar::from_u32(index)
+        let var = OrderVar::from_u32(index);
+        self.nodes.push(OrderNodeData {
+            bound: ConstantBound::None,
+            exprs: vec![var],
+        })
     }
 
     /// Mark one node, plus an offset, as equal to the other.
@@ -162,62 +248,85 @@ impl OrderGraph {
 
     /// Mark a node as having a fixed constant value.
     pub fn constant(&mut self, a: OrderNode, value: u64) -> OrderResult<()> {
-        let node = &mut self.nodes[a];
-        if node.lo.is_some() && Some(value) < node.lo {
-            return Err(OrderGraphError::ConstantConflict(a));
-        }
-        if node.hi.is_some() && Some(value) > node.hi {
-            return Err(OrderGraphError::ConstantConflict(a));
-        }
-        node.lo = Some(value);
-        node.hi = Some(value);
+        let new = ConstantBound::merge(self.nodes[a].bound, ConstantBound::Eq(value))
+            .ok_or_else(|| OrderGraphError::ConstantConflict(a))?;
+        self.nodes[a].bound = new;
         Ok(())
     }
 
-    /// Mark a node as having a fixed lower or upper (or both)
-    /// constant-value bound. Both ends are inclusive.
-    pub fn bounded(&mut self, a: OrderNode, lo: Option<u64>, hi: Option<u64>) -> OrderResult<()> {
-        let node = &mut self.nodes[a];
-        let lo = match (node.lo, lo) {
-            (None, lo) | (lo, None) => lo,
-            (Some(existing), Some(new)) => Some(std::cmp::max(existing, new)),
-        };
-        node.lo = lo;
-        let hi = match (node.hi, hi) {
-            (None, hi) | (hi, None) => hi,
-            (Some(existing), Some(new)) => Some(std::cmp::min(existing, new)),
-        };
-        node.hi = hi;
-
-        if let (Some(lo), Some(hi)) = (node.lo, node.hi) {
-            if lo > hi {
-                return Err(OrderGraphError::ConstantConflict(a));
-            }
-        }
+    /// Mark a node as having a fixed upper constant-value bound
+    /// (inclusive).
+    pub fn le_constant(&mut self, a: OrderNode, hi: u64) -> OrderResult<()> {
+        let new = ConstantBound::merge(self.nodes[a].bound, ConstantBound::Le(hi))
+            .ok_or_else(|| OrderGraphError::ConstantConflict(a))?;
+        self.nodes[a].bound = new;
         Ok(())
     }
 
     /// Simplify the order graph, potentially finding a contradiction
     /// and returning an error.
     pub fn simplify(&mut self) -> OrderResult<()> {
-        while self.simplify_iter()? {}
+        const MAX_ITERS: usize = 10;
+        
+        let mut uf = UnionFind::with_capacity(self.nodes.len());
+        for node_idx in self.nodes.keys() {
+            uf.add(node_idx);
+        }
+        let mut merged = vec![];
+        let mut iters = 0;
+        while self.simplify_iter(&mut uf, &mut merged)? && iters < MAX_ITERS {
+            iters += 1;
+            merged.sort_unstable();
+            merged.dedup();
+            for merged_node in merged.drain(..) {
+                let merged_into = uf.find_and_update(merged_node);
+                if merged_into == merged_node {
+                    continue;
+                }
+                let bound = self.nodes[merged_node].bound;
+                let bound = ConstantBound::merge(bound, self.nodes[merged_into].bound)
+                    .ok_or_else(|| OrderGraphError::ConstantConflict(merged_into))?;
+                self.nodes[merged_into].bound = bound;
+                let exprs = std::mem::take(&mut self.nodes[merged_node].exprs);
+                self.nodes[merged_into].exprs.extend(exprs.into_iter());
+                self.nodes[merged_into].exprs.sort_unstable();
+                self.nodes[merged_into].exprs.dedup();
+            }
+        }
         Ok(())
     }
 
     /// One fixpoint iteration of node simplification. Returns `true`
     /// if any node was updated or merged.
-    fn simplify_iter(&mut self) -> OrderResult<bool> {
+    fn simplify_iter(
+        &mut self,
+        uf: &mut UnionFind<OrderNode>,
+        merged: &mut Vec<OrderNode>,
+    ) -> OrderResult<bool> {
         let mut changed = false;
+
+        // Take all edges, rewrite endpoints according to union-find, and sort.
+        let mut edges = Vec::with_capacity(self.edges.len());
+        for edge in &self.edges {
+            let mut edge = edge.clone();
+            let (orig_lhs, orig_rhs) = (edge.lhs, edge.rhs);
+            edge.lhs = uf.find_and_update(edge.lhs);
+            edge.rhs = uf.find_and_update(edge.rhs);
+            changed |= orig_lhs != edge.lhs || orig_rhs != edge.rhs;
+            if edge.lhs != edge.rhs {
+                edges.push(edge);
+            }
+        }
+        edges.sort();
 
         // Merge edges where possible.  Iterate through edges grouped
         // by destination node. Note that `src` and `dest` are the
         // first fields in the edge struct, hence iteration order with
         // the derived `Ord` ensures we see edges between the same two
         // nodes sequentially.
-        let mut edges = std::mem::take(&mut self.edges);
         let mut new_edges = BTreeSet::new();
         let mut best_edge: Option<OrderEdge> = None;
-        for edge in &edges {
+        for edge in edges {
             if let Some(e) = best_edge {
                 if (edge.lhs, edge.rhs) != (e.lhs, e.rhs) {
                     new_edges.insert(e);
@@ -226,102 +335,70 @@ impl OrderGraph {
             }
             match best_edge {
                 None => {
-                    best_edge = Some(*edge);
+                    best_edge = Some(edge);
                 }
                 Some(cur) => {
-                    assert!(cur != *edge);
-                    match (cur.kind, edge.kind) {
-                        (OrderEdgeKind::Eq, OrderEdgeKind::Eq) => {
-                            return Err(OrderGraphError::ConflictingEdges(edge.lhs, edge.rhs));
-                        }
-                        (OrderEdgeKind::Eq, OrderEdgeKind::Le) if cur.offset < edge.offset => {
-                            return Err(OrderGraphError::ConflictingEdges(edge.lhs, edge.rhs));
-                        }
-                        (OrderEdgeKind::Le, OrderEdgeKind::Eq) if edge.offset < cur.offset => {
-                            return Err(OrderGraphError::ConflictingEdges(edge.lhs, edge.rhs));
-                        }
-                        (OrderEdgeKind::Eq, OrderEdgeKind::Le) => {
-                            best_edge = Some(cur);
-                        }
-                        (OrderEdgeKind::Le, OrderEdgeKind::Eq) => {
-                            best_edge = Some(*edge);
-                        }
-                        (OrderEdgeKind::Le, OrderEdgeKind::Le) => {
-                            best_edge = Some(OrderEdge {
-                                kind: OrderEdgeKind::Le,
-                                lhs: cur.lhs,
-                                rhs: cur.rhs,
-                                offset: std::cmp::max(cur.offset, edge.offset),
-                            });
-                        }
-                    }
+                    assert!(cur != edge);
+                    changed = true;
+                    best_edge =
+                        Some(OrderEdge::merge(&cur, &edge).ok_or_else(|| {
+                            OrderGraphError::ConflictingEdges(edge.lhs, edge.rhs)
+                        })?);
                 }
             }
         }
         if let Some(best_edge) = best_edge {
             new_edges.insert(best_edge);
         }
-        if new_edges != edges {
-            changed = true;
-        }
         self.edges = new_edges;
 
-        // Update each node's lo/hi range based on edges.
+        // Update each node's constant constraint based on edges.
         for edge in &self.edges {
             assert_ne!(edge.lhs, edge.rhs);
-            match edge.kind {
-                OrderEdgeKind::Le => {
-                    let (x, y) = unify_le(self.nodes[edge.lhs].lo, self.nodes[edge.rhs].lo);
-                    self.nodes[edge.lhs].lo = x;
-                    self.nodes[edge.rhs].lo = y;
-                    let (x, y) = unify_le(self.nodes[edge.lhs].hi, self.nodes[edge.rhs].hi);
-                    self.nodes[edge.lhs].hi = x;
-                    self.nodes[edge.rhs].hi = y;
-                }
-                OrderEdgeKind::Eq => {
-                    match (lhs_hi, rhs_lo) {
-                        (Some(a), Some(b)) => {
-                            if a.saturating_add(edge.offset) != b {
-                                return Err(OrderGraphError::ConflictingEdges(edge.lhs, edge.rhs));
-                            }
-                        }
-                        (Some(a), None) => {
-                            self.nodes[edge.rhs].lo
-                        }
+            let bound_rhs = OrderEdge::compose_left(&self.nodes[edge.lhs].bound, edge);
+            let bound_rhs = ConstantBound::merge(bound_rhs, self.nodes[edge.rhs].bound)
+                .ok_or_else(|| OrderGraphError::ConstantConflict(edge.rhs))?;
+            changed |= bound_rhs != self.nodes[edge.rhs].bound;
+            self.nodes[edge.rhs].bound = bound_rhs;
+            let bound_lhs = OrderEdge::compose_right(edge, &self.nodes[edge.rhs].bound);
+            let bound_lhs = ConstantBound::merge(bound_lhs, self.nodes[edge.lhs].bound)
+                .ok_or_else(|| OrderGraphError::ConstantConflict(edge.lhs))?;
+            changed |= bound_lhs != self.nodes[edge.lhs].bound;
+            self.nodes[edge.lhs].bound = bound_lhs;
+        }
+
+        // If any two nodes have an equality edge with offset 0, merge
+        // them.
+        for edge in &self.edges {
+            if edge.kind == OrderEdgeKind::Eq && edge.offset == 0 {
+                changed = true;
+                uf.union(edge.lhs, edge.rhs);
+                merged.push(edge.lhs);
+                merged.push(edge.rhs);
+            }
+        }
+
+        // If any two nodes have the same constant value, merge them.
+        let mut const_values = FxHashMap::default();
+        for (node, data) in self.nodes.iter() {
+            if let ConstantBound::Eq(x) = data.bound {
+                match const_values.entry(x) {
+                    Entry::Vacant(v) => {
+                        v.insert(node);
+                    }
+                    Entry::Occupied(o) => {
+                        let orig = *o.get();
+                        uf.union(orig, node);
+                        merged.push(orig);
+                        merged.push(node);
                     }
                 }
             }
         }
 
-        
-        for node in self.nodes.keys() {
-            let mut hi = self.nodes[node].hi;
-            // Invariant: we'll have at most one edge per dest node,
-            // so deferring dest nodes' `lo` updates is safe (we won't
-            // have two conflicting updates).
-            for edge in &self.nodes[node].edges {
-                if let Some(offset_hi) = hi.map(|val| val.saturating_add(edge.offset)) {
-                    let dest_lo = self.nodes[edge.dest].lo.unwrap_or(offset_hi);
-                    todo!()
-                }
-            }
-
-            if hi != self.nodes[node].hi || lo_updates.len() > 0 {
-                changed = true;
-            }
-            self.nodes[node].hi = hi;
-            for (node, lo) in lo_updates.drain(..) {
-                self.nodes[node].lo = Some(lo);
-            }
-        }
-
-        // If any two nodes have the same constant value, merge them.
-
-        // For each node, if outgoing edges imply two destination
-        // nodes are exactly equal, merge them.
-
-        // Look for inequality cycles with nonzero distance and flag
-        // an error (contradiction) if so.
+        // TODO: look for inequality cycles with nonzero distance and
+        // flag an error (contradiction) if so. Inequality cycles with
+        // zero distance merge all the nodes in the cycle.        
 
         Ok(changed)
     }
